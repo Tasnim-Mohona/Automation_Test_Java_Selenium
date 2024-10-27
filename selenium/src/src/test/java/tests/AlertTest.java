ad145394036ee009d157a9bed9ac86bc3d964c7a
qmohona <qmohona@gmail.com>
2024-10-27 12:33:51 +0600

    Added files from java_unit_selenium-master and alert test is done.

---
+++ selenium/src/src/test/java/tests/AlertTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AlertTest extends BaseTest {
+    private final By jsAlert = By.xpath(String.format(PRECISE_TEXT_XPATH, "JavaScript Alerts"));
+    private final By clickForJsAlertButton = By.xpath("//button[@onclick='jsAlert()']");
+    private final By resultAfterClickedOk = By.xpath("//p[@id='result']");
+    String successMessage = "You successfully clicked an alert";
+
+    @Test
+    public void alertTest() {
+        driver.findElement(jsAlert).click();
+        driver.findElement(clickForJsAlertButton).click();
+        driver.switchTo().alert().accept();
+        String text = driver.findElement(resultAfterClickedOk).getText();
+        Assert.assertEquals(text, successMessage);
+        // todo: accept alert
+        // todo: assert success message
+    }
+}
