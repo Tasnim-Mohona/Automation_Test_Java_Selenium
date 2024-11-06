44743506b3df05a1ffbf1bec83c5675fca5b52e0
qmohona <qmohona@gmail.com>
2024-11-06 11:50:01 +0600

    Dynamic Controls, Remove and Enable button is clicked, it waits until remove and disable buttons are shown and assertion assures their presence.

--- selenium/src/src/test/java/DynamicControlsTest.java
+++ selenium/src/src/test/java/DynamicControlsTest.java
@@ -1,14 +1,33 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class DynamicControlsTest extends BaseTest {
     private final By dynamicControls = By.xpath(String.format(PRECISE_TEXT_XPATH, "Dynamic Controls"));
-    private final By enable = By.xpath(String.format(PRECISE_TEXT_XPATH, "Enable"));
+    private final By enableButton = By.xpath(String.format(PRECISE_TEXT_XPATH, "Enable"));
+    private final By disableButton = By.xpath(String.format(PRECISE_TEXT_XPATH, "Enable"));
+    private final By removeButton = By.xpath("//button[@onclick = 'swapCheckbox()']");
+    private final By addButton = By.xpath(String.format(PRECISE_TEXT_XPATH, "Add"));
+    int WAIT_TIME = 10;
 
     @Test
     public void dynamicControlsTest() {
         driver.findElement(dynamicControls).click();
-        driver.findElement(enable).click();
+
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIME));
+        driver.findElement(removeButton).click();
+        wait.until(d -> driver.findElement(addButton).isDisplayed());
+        Assert.assertTrue(wait.until(d -> driver.findElement(addButton).isDisplayed()), "Add Button not displayed" );
+
+        driver.findElement(enableButton).click();
+        Assert.assertTrue(wait.until(d -> driver.findElement(disableButton).isDisplayed()),"It is enabled message is shown");
+
         // todo: assert input is enabled
         // todo: input random generated text
         // todo: assert inputted text
